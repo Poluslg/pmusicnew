fb6843393b42cd022059262ab7c1c1300750fed7
Poluslg <prahladbiswas08@gmail.com>
2023-05-18 10:42:15 +0530

    second

--- src/Components/MainLogin.jsx
+++ src/Components/MainLogin.jsx
@@ -1,144 +1,114 @@
-
-import { Link } from "react-router-dom";
-
-
-export default function Login() {
-
-
+export default function MainLogin() {
   return (
     <>
-        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-          <div className="w-full max-w-md space-y-8">
-            <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src="/favicon.ico"
-                alt="PMusic"
-              />
-              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Sign in to your account
-              </h2>
-            </div>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="/favicon.ico"
+              alt="PMusic"
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
 
-            <form className="-space-y-px rounded-md shadow-sm"
+          <form
+            className="-space-y-px rounded-md shadow-sm"
             //  onSubmit={login}
-             >
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Email address"
+          >
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Email address"
                 //   value={email}
                 //   onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
+              />
+            </div>
 
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Password"
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
                 //   value={password}
                 //   onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-            </form>
-            {/* <div className="lerror">
+              />
+            </div>
+          </form>
+          {/* <div className="lerror">
               <h1>{error}</h1>
             </div> */}
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
-
-              {/* <div className="text-sm">
-                <Link
-                  href="/forgotpassword"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot your password?
-                </Link>
-              </div> */}
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              />
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                Remember me
+              </label>
             </div>
 
-            <div className=" logos w-full">
-              <div className="googlelogoborder">
-                <button type="submit" 
-                // onClick={gsing}
-                >
-                  Connect with google
-                </button>
-                <img
-                  src="/google.svg"
-                  alt="Google-Login"
-                  className="googlelogo"
-                />
-              </div>
-
-              {/* <div className="spotifylogoborder">
-                <button type="submit" onClick={slogin}>
-                  Connect with Spotify
-                </button>
-                <img
-                  src="/spotify.png"
-                  alt="Google-Login"
-                  className="spotifylogo"
-                />
-              </div> */}
+            <div className="text-sm">
+              <a
+                href="/forgotpassword"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
             </div>
-            <div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              // onClick={login}
+              className="group relative flex w-full justify-center rounded-md border  bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3"></span>
+              Sign in
+            </button>
+            <div className="flex items-center justify-between pt-4 ">
               <button
                 type="submit"
-                // onClick={login}
-                className="group relative flex w-full justify-center rounded-md border  bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3"></span>
-                Sign in
-              </button>
-              <div className="btnac">
-                <button
-                  type="submit"
                 //   onClick={newac}
-                  className="group relative flex w-full justify-center rounded-md border  bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Create a new Account
-                </button>
-                <button
-                  type="submit"
+                className="group relative flex w-full justify-center font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Create a new Account
+              </button>
+              <button
+                type="submit"
                 //   onClick={cancel}
-                  className="group relative flex w-full justify-center rounded-md border  bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Cancel
-                </button>
-              </div>
+                className=" relative flex w-full justify-center font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Cancel
+              </button>
             </div>
           </div>
         </div>
+      </div>
     </>
   );
 }
